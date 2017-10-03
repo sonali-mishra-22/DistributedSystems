0b130d72cca9a1a61737e26b7e8fa34eed6911b1
sonali-mishra-22 <32229218+sonali-mishra-22@users.noreply.github.com>
2017-10-02 18:54:33 -0700

    Update Buffer.java

--- hw3/Buffer.java
+++ hw3/Buffer.java
@@ -2,14 +2,10 @@ package edu.dt;
 
 import java.util.Observable;
 
-/**
- * Created by tphadke on 8/29/17.
- */
 public class Buffer extends Observable {
     private Message message;
 
     public Buffer(){
-        //Create an empty Buffer
     }
     public Buffer(Message message) {
         this.message = message;
